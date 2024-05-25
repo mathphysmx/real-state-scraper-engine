078deed5f3c0b789d5797de4036dd6c4d042e2ae
Francisco Mendoza <mentofran@gmail.com>
2024-05-25 11:15:10 -0600

    test: :white_check_mark: get coordinates from http response

---
+++ tests/test_mercadolibrerealstatescraper.py
@@ -0,0 +1,9 @@
+from mercadolibrerealstatescraper import lat_long_zip_available
+
+def test_lat_long_zip_available():
+    target_value = 40.7
+    coord = lat_long_zip_available(
+        x = {'latitude': target_value,
+            'longitude': -74.0})
+    assert coord == target_value
+
